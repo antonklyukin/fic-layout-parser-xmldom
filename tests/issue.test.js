8c711ba8c794f520b6fb605e5cba3be503ccf266
Anton Klyukin <antonklyukin@gmail.com>
2023-07-16 19:30:39 +0300

    getJournalName() Tests Finished

--- tests/issue.test.js
+++ tests/issue.test.js
@@ -1,10 +1,24 @@
-let utils = require('../src/utils.js');
-let issue = require('../src/issue.js');
+const utils = require('../src/utils.js');
+const issue = require('../src/issue.js');
 
-let eaDocument = utils.getMockDocument('ia-2019-11');
-
-test('getJournalName(ia-2019-11) Testing', () => {
-  expect(issue.getJournalName(eaDocument)).toBe(
-    'Международный бухгалтерский учет'
-  );
+describe('getJournalName() Testing', () => {
+  it('getJournalName(ia-2019-11) Testing', () => {
+    expect.hasAssertions();
+    let eaDocument = utils.getMockDocument('ia-2019-11');
+    expect(issue.getJournalName(eaDocument)).toBe(
+      'Международный бухгалтерский учет'
+    );
+  });
+  it('getJournalName(ea-2023-05) Testing', () => {
+    expect.hasAssertions();
+    let eaDocument = utils.getMockDocument('ea-2023-05');
+    expect(issue.getJournalName(eaDocument)).toBe(
+      'Экономический анализ: теория и практика'
+    );
+  });
+  it('getJournalName(fc-2023-01) Testing', () => {
+    expect.hasAssertions();
+    let eaDocument = utils.getMockDocument('fc-2023-01');
+    expect(issue.getJournalName(eaDocument)).toBe('Финансы и кредит');
+  });
 });
