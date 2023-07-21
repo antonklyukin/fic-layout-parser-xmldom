770b1bd029cd6bd26a24c3c93383d264a57ead25
Anton Klyukin <antonklyukin@gmail.com>
2023-07-21 17:56:48 +0300

    Almost done with issue.js module

--- src/issue.js
+++ src/issue.js
@@ -22,6 +22,16 @@ const journalAcronyms = {
   'Дайджест-Финансы': 'df',
 };
 
+const journalTomeNumbersIn2016 = {
+  'Финансы и кредит': '22',
+  'Экономический анализ: теория и практика': '15',
+  'Региональная экономика: теория и практика': '14',
+  'Национальные интересы: приоритеты и безопасность': '12',
+  'Финансовая аналитика: проблемы и решения': '9',
+  'Международный бухгалтерский учет': '19',
+  'Дайджест-Финансы': '21',
+};
+
 /**
  * Функция возвращает название журнала по найденному в макете выпуска ISSN
  * @function
@@ -39,30 +49,109 @@ const getJournalName = (document) => {
       }
     }
   }
+  return null;
+};
+
+/**
+ * Функция возвращает объект с данными о датах публикации выпуска
+ * @function
+ * @param {Document} document
+ * @returns {Object|null}
+ */
+
+const getPubDates = (document) => {
+  const pubDates = {};
+  const regexp = new RegExp(
+    [
+      'Подписано в печать (\\d\\d.\\d\\d.\\d\\d\\d\\d)\\s?',
+      'Выход в свет (\\d\\d.\\d\\d.(\\d\\d\\d\\d))',
+    ].join('')
+  );
+  const tableCells = document.getElementsByTagName('table:table-cell');
+  // console.log(tableCells.length);
+  for (let i = 0; i < tableCells.length; i++) {
+    let cellText = utils.getTextContentFromNode(tableCells[i]);
+    cellText = utils.clearString(cellText);
+    const found = cellText.match(regexp);
+    if (found) {
+      pubDates.pubDate = found[1];
+      pubDates.releaseDate = found[2];
+      pubDates.releaseYear = found[3];
+      return pubDates;
+    }
+  }
+  return null;
+};
+
+const getjournalTomeNumberIn2016 = (document) => {
+  const journalName = getJournalName(document);
+  return journalTomeNumbersIn2016[journalName];
 };
 
 /**
  * Функция возвращает объект с найденными номером тома, номером выпуска и годом выпуска
  * @function
  * @param {Document} document
- * @returns
+ * @returns {Object|null}
  */
 const getPublicationData = (document) => {
-  let pubData = {};
-  const regexp = /^ТОМ\s+(\d+),\s+ВЫПУСК\s+(\d+)(\D+)(\d+)/;
-  let tableCells = document.getElementsByTagName('table:table-cell');
-  for (let i = 0; i < tableCells.length; i++) {
-    let cellText = utils.getTextContentFromNode(tableCells[i]);
-    let found = cellText.match(regexp);
-    if (found) {
-      pubData.tome = found[1];
-      pubData.issue = found[2];
-      pubData.month = found[3];
-      pubData.year = found[4];
+  const releaseYear = getPubDates(document).releaseYear;
+
+  if (releaseYear !== '2016') {
+    const pubData = {};
+    const regexp = /^ТОМ\s(\d+),\sВЫПУСК\s(\d+)(\D+)(\d+)/;
+    const tableCells = document.getElementsByTagName('table:table-cell');
+    for (let i = 0; i < tableCells.length; i++) {
+      let cellText = utils.getTextContentFromNode(tableCells[i]);
+      cellText = utils.clearString(cellText);
+      const found = cellText.match(regexp);
+      if (found) {
+        pubData.tome = found[1];
+        pubData.issue = found[2].trim();
+        pubData.month = String(found[3]).toLowerCase().trim(); //Убираем пробел после regex
+        pubData.year = found[4];
+
+        return pubData;
+      }
+    }
+  } else {
+    const pubData = {};
+    const regexp = /(\d+)\s\((\d+)\)\s?(\D+)\s(\d\d\d\d)/;
+    const tableCells = document.getElementsByTagName('table:table-cell');
+    for (let i = 0; i < tableCells.length; i++) {
+      let cellText = utils.getTextContentFromNode(tableCells[i]);
+      cellText = utils.clearString(cellText);
+      const found = cellText.match(regexp);
+      if (found) {
+        pubData.tome = getjournalTomeNumberIn2016(document);
+        pubData.issue = found[1];
+        pubData.month = String(found[3]).toLowerCase().trim(); //Убираем пробел после regex
+        pubData.year = found[4];
+
+        return pubData;
+      }
     }
   }
-
-  return pubData;
+  return null;
 };
 
-module.exports = { getJournalName, getPublicationData };
+const getThroughIssueNumber = (document) => {
+  const regexp = /Валовый\s+\(сквозной\)\s+номер\s+(\d+)[\s\r]+Формат/;
+  const tableCells = document.getElementsByTagName('table:table-cell');
+  for (let i = 0; i < tableCells.length; i++) {
+    let cellText = utils.getTextContentFromNode(tableCells[i]);
+    cellText = utils.clearString(cellText);
+    let found = cellText.match(regexp);
+    if (found) {
+      return found[1];
+    }
+  }
+  return null;
+};
+
+module.exports = {
+  getJournalName,
+  getPubDates,
+  getPublicationData,
+  getThroughIssueNumber,
+};
